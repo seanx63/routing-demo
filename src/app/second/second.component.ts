ca8d5f6e552844af3805ffd207db1df704307f28
Sean Gates <seanx63@gmail.com>
2022-11-15 10:10:12 -0700

    routing

---
+++ src/app/second/second.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-second',
+  templateUrl: './second.component.html',
+  styleUrls: ['../first/first.component.css']
+})
+export class SecondComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
